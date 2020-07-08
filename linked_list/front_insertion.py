b1bfbf32f84c4ae60a946f6829d1db8250f4e93f
sathya narayanan <sathyanarayanan741@gmai.com>
2020-07-08 18:42:19 +0530

    front insertion added

---
+++ linked_list/front_insertion.py
@@ -0,0 +1,32 @@
+#inserting a node at very first of the list
+
+class node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+class Linkedlist:
+    def __init__(self):
+        self.head = None
+    
+    def before_insertion_print(self):
+        head = self.head
+        while(head):
+            print(head.data)
+            head  = head.next
+
+#driver code
+llist = Linkedlist()
+
+#creating three empty nodes
+llist.head = node(1)
+second = node(2)
+third = node(3)
+
+#linking all the nodes together
+llist.head.next = second
+second.next = third
+
+#printing the linked list before insertion
+print("Printing the linked list before insertion...... ")
+llist.before_insertion_print()
